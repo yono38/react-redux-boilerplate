53c914a9bdc84acda509f78ed633c2388cffd32a
Jason Schapiro <yono38@gmail.com>
2016-10-23 00:52:55 -0400

    chore(eslint): Add eslint

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+/* global window, document */
+/* eslint no-render-return-value: 0, jsx-filename-extension: 0 */
+
 // index.js
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import Counter from './components/Counter'
-import counter from './reducers'
-import { INCREMENT, DECREMENT } from './actions'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import Counter from './components/Counter';
+import counter from './reducers';
+import { INCREMENT, DECREMENT } from './actions';
 
-const store = createStore(counter);
+/* eslint-disable no-underscore-dangle */
+const store = createStore(counter,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__());
+/* eslint-enable */
 
 const render = () => ReactDOM.render(
   <Counter
@@ -16,7 +23,7 @@ const render = () => ReactDOM.render(
     onDecrement={() => store.dispatch({ type: DECREMENT })}
   />,
   document.getElementById('root')
-)
+);
 
 render();
-store.subscribe(render)
+store.subscribe(render);
--- reducers/index.js
+++ reducers/index.js
@@ -1,13 +1,14 @@
 // reducers/index.js
-import { INCREMENT, DECREMENT } from '../actions'
+import { INCREMENT, DECREMENT } from '../actions';
+
 const initialState = 0;
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return state + 1
+      return state + 1;
     case DECREMENT:
-      return state - 1
+      return state - 1;
     default:
-      return state
+      return state;
   }
-}
+};
